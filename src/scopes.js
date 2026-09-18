316ea365473c2aadd635826e175fa8915e6aba06
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-18 03:13:54 -0500

    style: js-beautify formatting (src/scopes.js)

--- src/scopes.js
+++ src/scopes.js
@@ -1,39 +1,215 @@
 'use strict';
 
 const profiles = {
-  javascript: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'", '`'] },
-  javascriptreact: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'", '`'] },
-  typescript: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'", '`'] },
-  typescriptreact: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'", '`'] },
-  java: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  c: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  cpp: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  csharp: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  go: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'", '`'] },
-  rust: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  groovy: { line: ['//'], block: [['/*', '*/']], quotes: ['"""', "'''", '"', "'"] },
-  jenkinsfile: { line: ['//'], block: [['/*', '*/']], quotes: ['"""', "'''", '"', "'"] },
-  gradle: { line: ['//'], block: [['/*', '*/']], quotes: ['"""', "'''", '"', "'"] },
-  css: { line: [], block: [['/*', '*/']], quotes: ['"', "'"] },
-  scss: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  less: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  json: { line: [], block: [], quotes: ['"'] },
-  jsonc: { line: ['//'], block: [['/*', '*/']], quotes: ['"'] },
-  toml: { line: ['#'], block: [], quotes: ['"""', "'''", '"', "'"] },
-  ini: { line: [';', '#'], block: [], quotes: ['"', "'"] },
-  properties: { line: ['#', '!'], block: [], quotes: ['"', "'"] },
-  dotenv: { line: ['#'], block: [], quotes: ['"', "'"] },
-  conf: { line: ['#', ';'], block: [], quotes: ['"', "'"] },
-  python: { line: ['#'], block: [], quotes: ['"""', "'''", '"', "'"] },
-  ruby: { line: ['#'], block: [], quotes: ['"', "'"] },
-  shellscript: { line: ['#'], block: [], quotes: ['"', "'", '`'] },
-  yaml: { line: ['#'], block: [], quotes: ['"', "'"] },
-  dockerfile: { line: ['#'], block: [], quotes: ['"', "'"] },
-  html: { line: [], block: [['<!--', '-->']], quotes: ['"', "'"] },
-  xml: { line: [], block: [['<!--', '-->']], quotes: ['"', "'"] },
-  php: { line: ['//', '#'], block: [['/*', '*/']], quotes: ['"', "'"] },
-  swift: { line: ['//'], block: [['/*', '*/']], quotes: ['"'] },
-  kotlin: { line: ['//'], block: [['/*', '*/']], quotes: ['"', "'"] },
+  javascript: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'", '`']
+  },
+  javascriptreact: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'", '`']
+  },
+  typescript: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'", '`']
+  },
+  typescriptreact: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'", '`']
+  },
+  java: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  c: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  cpp: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  csharp: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  go: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'", '`']
+  },
+  rust: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  groovy: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"""', "'''", '"', "'"]
+  },
+  jenkinsfile: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"""', "'''", '"', "'"]
+  },
+  gradle: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"""', "'''", '"', "'"]
+  },
+  css: {
+    line: [],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  scss: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  less: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  json: {
+    line: [],
+    block: [],
+    quotes: ['"']
+  },
+  jsonc: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"']
+  },
+  toml: {
+    line: ['#'],
+    block: [],
+    quotes: ['"""', "'''", '"', "'"]
+  },
+  ini: {
+    line: [';', '#'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  properties: {
+    line: ['#', '!'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  dotenv: {
+    line: ['#'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  conf: {
+    line: ['#', ';'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  python: {
+    line: ['#'],
+    block: [],
+    quotes: ['"""', "'''", '"', "'"]
+  },
+  ruby: {
+    line: ['#'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  shellscript: {
+    line: ['#'],
+    block: [],
+    quotes: ['"', "'", '`']
+  },
+  yaml: {
+    line: ['#'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  dockerfile: {
+    line: ['#'],
+    block: [],
+    quotes: ['"', "'"]
+  },
+  html: {
+    line: [],
+    block: [
+      ['<!--', '-->']
+    ],
+    quotes: ['"', "'"]
+  },
+  xml: {
+    line: [],
+    block: [
+      ['<!--', '-->']
+    ],
+    quotes: ['"', "'"]
+  },
+  php: {
+    line: ['//', '#'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
+  swift: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"']
+  },
+  kotlin: {
+    line: ['//'],
+    block: [
+      ['/*', '*/']
+    ],
+    quotes: ['"', "'"]
+  },
 };
 
 function earliestAt(text, index, candidates) {
@@ -68,7 +244,10 @@ function getColorableSpans(text, languageId) {
     if (lineStart) {
       const end = text.indexOf('\n', index + lineStart.length);
       const stop = end === -1 ? text.length : end;
-      spans.push({ start: index, end: stop });
+      spans.push({
+        start: index,
+        end: stop
+      });
       index = stop;
       continue;
     }
@@ -77,7 +256,10 @@ function getColorableSpans(text, languageId) {
     if (block) {
       const end = text.indexOf(block[1], index + block[0].length);
       const stop = end === -1 ? text.length : end + block[1].length;
-      spans.push({ start: index, end: stop });
+      spans.push({
+        start: index,
+        end: stop
+      });
       index = stop;
       continue;
     }
@@ -85,7 +267,10 @@ function getColorableSpans(text, languageId) {
     const quote = earliestAt(text, index, profile.quotes);
     if (quote) {
       const stop = findQuotedEnd(text, index, quote);
-      spans.push({ start: index, end: stop });
+      spans.push({
+        start: index,
+        end: stop
+      });
       index = stop;
       continue;
     }
@@ -96,4 +281,7 @@ function getColorableSpans(text, languageId) {
   return spans;
 }
 
-module.exports = { getColorableSpans, profiles };
+module.exports = {
+  getColorableSpans,
+  profiles
+};
