3bae84317a4ddc20f4d26656fbf76e0341fe471c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-18 23:57:17 -0500

    style: js-beautify formatting (test/web.test.js)

--- test/web.test.js
+++ test/web.test.js
@@ -9,31 +9,72 @@ const {
 } = require('../web/colorizer');
 
 function colored(text) {
-  return colorize(text).filter(({ color }) => color);
+  return colorize(text).filter(({
+    color
+  }) => color);
 }
 
-assert.deepEqual(colored(String.raw`\b B blue Blueberry table`), [
-  { text: String.raw`\b`, color: '#4e4eff' },
-  { text: 'B', color: '#4e4eff' },
-  { text: 'blue', color: '#4e4eff' },
-  { text: 'Blueberry', color: '#4e4eff' },
+assert.deepEqual(colored(String.raw`\b B blue Blueberry table`), [{
+    text: String.raw`\b`,
+    color: '#4e4eff'
+  },
+  {
+    text: 'B',
+    color: '#4e4eff'
+  },
+  {
+    text: 'blue',
+    color: '#4e4eff'
+  },
+  {
+    text: 'Blueberry',
+    color: '#4e4eff'
+  },
 ]);
-assert.deepEqual(colored('{[()]}<>*•42'), [
-  { text: '{', color: '#ff79c6' },
-  { text: '[', color: '#ba7dff' },
-  { text: '()', color: '#ffa500' },
-  { text: ']', color: '#ba7dff' },
-  { text: '}', color: '#ff79c6' },
-  { text: '<>', color: '#ffa500' },
-  { text: '*•', color: '#6495ed' },
-  { text: '42', color: '#00bfff' },
+assert.deepEqual(colored('{[()]}<>*•42'), [{
+    text: '{',
+    color: '#ff79c6'
+  },
+  {
+    text: '[',
+    color: '#ba7dff'
+  },
+  {
+    text: '()',
+    color: '#ffa500'
+  },
+  {
+    text: ']',
+    color: '#ba7dff'
+  },
+  {
+    text: '}',
+    color: '#ff79c6'
+  },
+  {
+    text: '<>',
+    color: '#ffa500'
+  },
+  {
+    text: '*•',
+    color: '#6495ed'
+  },
+  {
+    text: '42',
+    color: '#00bfff'
+  },
 ]);
 assert.equal(hueRotatedGreen('+'), '#00ff26');
 assert.equal(hueRotatedGreen('😀'), '#ff69bb');
 assert.equal(colored('😀')[0].color, hueRotatedGreen('😀'));
-assert.deepEqual(colored('apple123 _blue xylophone'), [
-  { text: '123', color: '#00bfff' },
-  { text: '_', color: hueRotatedGreen('_') },
+assert.deepEqual(colored('apple123 _blue xylophone'), [{
+    text: '123',
+    color: '#00bfff'
+  },
+  {
+    text: '_',
+    color: hueRotatedGreen('_')
+  },
 ]);
 assert.match(
   clipboardHtml('<Blue & red>'),
@@ -44,8 +85,8 @@ assert.doesNotMatch(clipboardHtml('<Blue & red>'), /<Blue/);
 assert.match(clipboardHtml('a\nb'), /white-space:pre-wrap/);
 assert.doesNotMatch(clipboardHtml('plain'), /color:#e6edf3|background/);
 
-const helm = 'component: {{ printf "%s-%s" .Values.frost.nameOverride '
-  + '.Values.frost.instance | trunc 63 | trimSuffix "-" | quote }}';
+const helm = 'component: {{ printf "%s-%s" .Values.frost.nameOverride ' +
+  '.Values.frost.instance | trunc 63 | trimSuffix "-" | quote }}';
 const helmHtml = clipboardHtml(helm);
 for (const expectedColor of ['#b31972', '#7132b8', teamsColor('#00f8ff'), '#006f99', teamsColor('#d6c700')]) {
   assert.match(helmHtml, new RegExp(`<font color="${expectedColor}">`));
